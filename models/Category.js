10758c923750030b2ab47c04fd58e1af5517c6f3
James Jackson <jjackson0228@gmail.com>
2024-08-25 19:59:29 -0400

    added category-routes.js and Category.js defined columns and the api/categories endpoint

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns added a category 25 aug 7:58
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
