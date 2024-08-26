34d95bfd5ebf2aff762f18fcc81e8774b7473093
James Jackson <jjackson0228@gmail.com>
2024-08-25 20:43:09 -0400

    defined ProductTag in the ProductTag.js in the models folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    //defined ProductTag by id, product_id, onDelete, and tag_id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
   },
   {
     sequelize,
